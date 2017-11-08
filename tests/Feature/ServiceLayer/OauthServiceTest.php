f86a28d7a72ecac8d410c1f8c1c466400daf8443
Carwyn Stephen <caztephen@gmail.com>
2017-11-08 22:19:48 +0100

    Stub Oauth Service Test

---
+++ tests/Feature/ServiceLayer/OauthServiceTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Feature\ServiceLayer;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class OauthServiceTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+    }
+}
