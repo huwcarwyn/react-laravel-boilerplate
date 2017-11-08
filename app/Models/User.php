c2a5a2630085d2a8572be64b4100c24c1cf921c5
Carwyn Stephen <caztephen@gmail.com>
2017-11-08 22:12:20 +0100

    Add Password Hashing as Mutator
    Change Full Name to First/Last Name

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,19 +2,25 @@
 
 namespace App\Models;
 
-use Laravel\Passport\HasApiTokens;
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Passport\HasApiTokens,
+    Illuminate\Notifications\Notifiable,
+    Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
     use HasApiTokens, Notifiable;
 
     protected $fillable = [
-        'name', 'email', 'password',
+        'first_name', 'last_name', 'email', 'password',
     ];
 
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function setPasswordAttribute($password)
+    {
+      $hash = resolve('Illuminate\Contracts\Hashing\Hasher');
+      $this->attributes['password'] = $hash->make($password);
+    }
 }
