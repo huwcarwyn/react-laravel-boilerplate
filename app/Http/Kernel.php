bce455462cc362dfa216cd21e5f971be6975f786
Carwyn Stephen <caztephen@gmail.com>
2017-11-19 23:30:49 +0100

    Ensure laravel_token is encrypted

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -30,7 +30,6 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
-            // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class
@@ -56,5 +55,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'encryptCookies' => \App\Http\Middleware\EncryptCookies::class,
     ];
 }
--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,9 @@
 
 use Illuminate\Http\Request;
 
-Route::middleware(['auth:api'])->group(function() {
+Route::group(['middleware' => ['auth:api']], function() {
 	Route::get('/users/me', '\App\Api\Controllers\SessionController@currentUser');
-
-	Route::post('/login', '\App\Api\Controllers\UserController@login');
 });
 
-Route::post('/signup', '\App\Api\Controllers\UserController@signup');
+Route::post('/login', '\App\Api\Controllers\UserController@login');
+Route::post('/signup', '\App\Api\Controllers\UserController@signup')->middleware('encryptCookies');
