4473bbc8421f9d349e9bdac0e1d2a4ab35bdfc1a
Carwyn Stephen <caztephen@gmail.com>
2017-12-17 16:56:48 +0100

    Make Routes Point to Correct Controller Methods

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,8 @@ use Illuminate\Http\Request;
 
 Route::group(['middleware' => ['auth:api']], function() {
 	Route::get('/users/me', '\App\Api\Controllers\SessionController@currentUser');
+	Route::post('/logout', '\App\Api\Controllers\SessionController@logout');
 });
 
-Route::post('/login', '\App\Api\Controllers\UserController@login');
+Route::post('/login', '\App\Api\Controllers\SessionController@login');
 Route::post('/signup', '\App\Api\Controllers\UserController@signup')->middleware('encryptCookies');
