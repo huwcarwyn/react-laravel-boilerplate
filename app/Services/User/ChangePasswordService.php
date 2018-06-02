fd3705e722a13cdb709ad0712dacaa9eca8f3fda
Carwyn Stephen <caztephen@gmail.com>
2018-06-02 22:00:17 +0200

    Change password service

---
+++ app/Services/User/ChangePasswordService.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Services\User;
+
+use App\Contracts\Repository\UserRepositoryContract as UserRepository,
+    Illuminate\Contracts\Routing\ResponseFactory as Response,
+    Illuminate\Contracts\Validation\Factory as Validator,
+    Illuminate\Contracts\Hashing\Hasher;
+
+class ChangePasswordService
+{
+	private $repository;
+	private $response;
+	private $validator;
+	private $hasher;
+	private $user;
+
+	public function __construct(
+	    Validator $validator,
+	    UserRepository $repository,
+	    Response $response,
+	    Hasher $hasher)
+	{
+	    $this->repository = $repository;
+	    $this->response = $response;
+	    $this->validator = $validator;
+	    $this->hasher = $hasher;
+	}
+
+	public function makeValidator($data)
+	{
+	    return $this->validator->make($data, [
+	      'user_id'      => 'required|numeric',
+	      'old_password' => 'required',
+	      'new_password' => 'required|confirmed'
+	    ]);
+	}
+
+	public function changePassword($data)
+	{
+		$validator = $this->makeValidator($data);
+
+		if ($validator->fails())
+		{
+			return $this->response->validateError($validator->failed());
+		}
+
+		$this->user = $this->repository->find($data['user_id']);
+
+		if ($this->hasher->check($data['old_password'], $this->user->password))
+		{
+			$this->user->password = $data['new_password'];
+			$this->user->save();
+			
+			return $this->response->success(['message' => 'User password successfully updated']);
+		} else {
+			return $this->response->error("Old Password is incorrect");
+		}
+	}
+}
