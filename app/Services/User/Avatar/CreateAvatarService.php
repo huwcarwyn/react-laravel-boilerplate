3e3efc28336022c7d40732280e1b1239351ecc00
Carwyn Stephen <caztephen@gmail.com>
2018-06-26 21:00:37 +0200

    WIP create avatar service function

--- app/Services/User/Avatar/CreateAvatarService.php
+++ app/Services/User/Avatar/CreateAvatarService.php
@@ -2,7 +2,34 @@
 
 namespace App\Services\User\Avatar;
 
+use Illuminate\Contracts\Validation\Factory as Validator;
+
 class CreateAvatarService
 {
+  private $validator;
 
+  public function __construct(Validator $validator)
+  {
+    $this->validator = $validator;
+  }
+
+  public function makeAvatarValidator($data)
+  {
+    return $this->validator->make($data, [
+      'avatar' => 'mimetypes:image/jpeg,image/png,image/gif'
+    ]);
+  }
+
+  public function create($file)
+  {
+    $fileValidator = $this->makeValidator(['avatar' => $file]);
+
+    if ($fileValidator->fails()) {
+      return $this->response->validateError($fileValidator->failed());
+    }
+
+    // Create various file sizes using some lib
+
+    // Upload the files using flysystem
+  }
 }
