566be06d74dbbaab578bbf6f3e3e9b40fcb7b591
Carwyn Stephen <caztephen@gmail.com>
2018-07-08 23:46:21 +0200

    Add some avatar methods to UserRepository

--- app/Repositories/Eloquent/UserRepository.php
+++ app/Repositories/Eloquent/UserRepository.php
@@ -3,18 +3,49 @@
 namespace App\Repositories\Eloquent;
 
 use App\Models\User,
+    App\Presenters\UserPresenter,
     App\Criterias\OnlyOwnUserCriteria,
     Prettus\Repository\Eloquent\BaseRepository,
     App\Contracts\Repository\UserRepositoryContract;
 
 class UserRepository extends BaseRepository implements UserRepositoryContract
 {
+  private $auth;
+
   public function boot()
   {
+    $this->auth = resolve('Illuminate\Contracts\Auth\Factory');
+
     $this->pushCriteria(OnlyOwnUserCriteria::class);
   }
 
-  public function model() {
+  public function model()
+  {
     return User::class;
+  }
+
+	public function presenter()
+	{
+		return UserPresenter::class;
+  }
+
+  public function currentUser()
+  {
+    return $this->find($this->auth->user()->id);
+  }
+
+  public function setCurrentAvatar($fileUrl)
+  {
+    return $this->update(['avatar' => $fileUrl], $this->auth->user()->id);
+  }
+
+  public function getCurrentAvatarFile()
+  {
+    return $this->skipPresenter()->find($this->auth->user()->id)->avatar;
+  }
+
+  public function removeCurrentAvatar()
+  {
+    return $this->update(['avatar' => null], $this->auth->user()->id);
   }
 }
