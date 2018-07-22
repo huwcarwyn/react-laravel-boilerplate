4bace1abb5c0f33c392d02ff5109516c1783d36d
Carwyn Stephen <caztephen@gmail.com>
2018-07-22 19:44:46 +0200

    Add example modal to the overview page

--- resources/assets/js/pages/Overview/Overview.jsx
+++ resources/assets/js/pages/Overview/Overview.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
+import { NeutralButton } from 'components'
+import { ModalConsumer } from 'contexts'
+
 const OverviewComponent = (props) => {
+  const ModalExample = (props) => <div>{props.message}</div>
   return (
     <div>
       Put your initial dashboard page here
+      <div className="mt-4">
+        <ModalConsumer>
+          {
+            ({ showModal }) => (
+              <NeutralButton onClick={() => showModal(ModalExample, { message: 'This message was passed in via modal props' })}>
+                Open an example modal
+              </NeutralButton>
+            )
+          }
+        </ModalConsumer>
+      </div>
     </div>
   )
 }
