f2a196f769573a6a32c8416597c964d64a5649ee
Carwyn Stephen <caztephen@gmail.com>
2018-07-22 19:44:33 +0200

    Pass all props into button components

--- resources/assets/js/components/Button.jsx
+++ resources/assets/js/components/Button.jsx
@@ -4,24 +4,24 @@ const baseButtonStyles = 'bg-transparent font-semibold border hover:text-white h
 
 export const NeutralButton = (props) => (
   <button
-    type={props.type}
-    className={`${baseButtonStyles} hover:bg-blue text-blue-dark py-2 px-4 border-blue  ${props.className}`}>
+    {...props}
+    className={`${baseButtonStyles} hover:bg-blue text-blue-dark py-2 px-4 border-blue  ${props.className || ''}`}>
     {props.children}
   </button>
 )
 
 export const NegativeButton = (props) => (
   <button
-    type={props.type}
-    className={`${baseButtonStyles} hover:bg-red text-red-dark py-2 px-4 border-red ${props.className}`}>
+    {...props}
+    className={`${baseButtonStyles} hover:bg-red text-red-dark py-2 px-4 border-red ${props.className || ''}`}>
     {props.children}
   </button>
 )
 
 export const PositiveButton = (props) => (
   <button
-    type={props.type}
-    className={`${baseButtonStyles} hover:bg-green text-green-dark py-2 px-4 border-green ${props.className}`}>
+    {...props}
+    className={`${baseButtonStyles} hover:bg-green text-green-dark py-2 px-4 border-green ${props.className || ''}`}>
     {props.children}
   </button>
 )
