0f9df5874342a568dbeb0613d20cfab01d0575b7
Carwyn Stephen <caztephen@gmail.com>
2018-07-27 22:33:51 +0200

    Put modalroot inside router

--- resources/assets/js/app.jsx
+++ resources/assets/js/app.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Provider } from 'react-redux'
 import { Route, Switch } from 'react-router-dom'
 import { ConnectedRouter } from 'react-router-redux'
@@ -33,20 +33,22 @@ export const App = (props) => (
     <Provider store={store}>
       <div>
         <FlashMessageRoot />
-        <ModalRoot />
         <ConnectedRouter history={browserHistory}>
-          <Switch>
-            <Route exact path='/login' render={() => <FormPageLayout title="Log In"><LogIn /></FormPageLayout>} />
-            <Route exact path='/signup' render={() => <FormPageLayout title="Sign Up"><SignUp /></FormPageLayout>} />
-            <Route exact path='/forgot-password' render={() => <FormPageLayout title="Forgot Password"><ForgotPassword /></FormPageLayout>} />
-            <Route exact path='/reset-password/:resetToken' render={() => <FormPageLayout title="Reset Password"><PasswordReset /></FormPageLayout>} />
+          <Fragment>
+            <ModalRoot />
+            <Switch>
+              <Route exact path='/login' render={() => <FormPageLayout title="Log In"><LogIn /></FormPageLayout>} />
+              <Route exact path='/signup' render={() => <FormPageLayout title="Sign Up"><SignUp /></FormPageLayout>} />
+              <Route exact path='/forgot-password' render={() => <FormPageLayout title="Forgot Password"><ForgotPassword /></FormPageLayout>} />
+              <Route exact path='/reset-password/:resetToken' render={() => <FormPageLayout title="Reset Password"><PasswordReset /></FormPageLayout>} />
 
-            {/* Dashboard routes */}
-            <Route exact path='/' component={withDashboard(Overview)} />
-            <Route path='/settings' component={withDashboard(SettingsRoutes)} />
-            {/* 404 route */}
-            <Route path="*" exact={true} component={NotFound}/>
-          </Switch>
+              {/* Dashboard routes */}
+              <Route exact path='/' component={withDashboard(Overview)} />
+              <Route path='/settings' component={withDashboard(SettingsRoutes)} />
+              {/* 404 route */}
+              <Route path="*" exact={true} component={NotFound}/>
+            </Switch>
+          </Fragment>
         </ConnectedRouter>
       </div>
     </Provider>
