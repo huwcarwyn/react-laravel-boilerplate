511805ef8d8eeb2d8514861c0aa2d4ae530701c8
Carwyn Stephen <caztephen@gmail.com>
2018-11-10 16:07:51 +0100

    Use babel eslint parser for new import

--- resources/assets/js/app.jsx
+++ resources/assets/js/app.jsx
@@ -4,6 +4,8 @@ import { Route, Switch } from 'react-router-dom'
 import { ConnectedRouter } from 'react-router-redux'
 
 import { DashboardLayout, FormPageLayout } from 'layouts'
+import { store, browserHistory } from 'store/create-store'
+import { AuthGuard, FlashMessageRoot, ModalProviderWrapper, ModalRoot } from 'components'
 
 const LogIn = lazy(() => import('pages/LogIn/LogIn'))
 const SignUp = lazy(() => import('pages/SignUp/SignUp'))
@@ -12,10 +14,6 @@ const PasswordReset = lazy(() => import('pages/PasswordReset/PasswordReset'))
 const ForgotPassword = lazy(() => import('pages/ForgotPassword/ForgotPassword'))
 const NotFound = lazy(() => import('pages/NotFound/NotFound'))
 const SettingsRoutes = lazy(() => import('pages/Settings/SettingsRoutes'))
-
-import { AuthGuard, FlashMessageRoot, ModalProviderWrapper, ModalRoot } from 'components'
-
-import { store, browserHistory } from 'store/create-store'
 
 const withDashboard = (ContentComponent) => {
   return (props) => (
@@ -42,7 +40,7 @@ export const App = (props) => (
           <Fragment>
             <ModalRoot />
             <Switch>
-              <Route exact path='/login' render={() => {console.log(LogIn); return <FormPageLayout title="Log In"><LogIn /></FormPageLayout>}} />
+              <Route exact path='/login' render={() => <FormPageLayout title="Log In"><LogIn /></FormPageLayout>} />
               <Route exact path='/signup' render={() => <FormPageLayout title="Sign Up"><SignUp /></FormPageLayout>} />
               <Route exact path='/forgot-password' render={() => <FormPageLayout title="Forgot Password"><ForgotPassword /></FormPageLayout>} />
               <Route exact path='/reset-password/:resetToken' render={() => <FormPageLayout title="Reset Password"><PasswordReset /></FormPageLayout>} />
