b8933455d6c167337caff35a11c0ff0b19fae99e
Carwyn Stephen <caztephen@gmail.com>
2019-04-08 21:12:38 +0200

    Remove unused prop arg

--- resources/assets/js/components/Logo/Logo.jsx
+++ resources/assets/js/components/Logo/Logo.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export const Logo = props => (
+export const Logo = () => (
   <h1 className="text-lg">
     <Link className="text-white no-underline" to="/">
       Boilerplate
