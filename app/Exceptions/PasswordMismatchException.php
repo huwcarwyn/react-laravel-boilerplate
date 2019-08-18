51b94027437fc069457dc274e78018434f9c93f3
Carwyn Stephen <caztephen@gmail.com>
2019-08-18 19:23:15 +0200

    Fix password mismatch exception

--- app/Exceptions/PasswordMismatchException.php
+++ app/Exceptions/PasswordMismatchException.php
@@ -2,7 +2,9 @@
 
 namespace App\Exceptions;
 
-class PasswordMimatchExteption extends Exception
+use Exception;
+
+class PasswordMismatchException extends Exception
 {
     public function __construct($message, $code = 0, Exception $previous = null)
     {
