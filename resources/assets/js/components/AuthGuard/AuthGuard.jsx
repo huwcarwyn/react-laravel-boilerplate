18f09219816c888561d47efb465e56f6cec81fd5
Carwyn Stephen <caztephen@gmail.com>
2019-08-18 15:35:07 +0200

    Use hooks for auth guard

--- resources/assets/js/components/AuthGuard/AuthGuard.jsx
+++ resources/assets/js/components/AuthGuard/AuthGuard.jsx
@@ -1,66 +1,45 @@
-import React from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 import { replace } from 'react-router-redux'
 import { getCurrentUserInfo } from 'store/action-creators/session'
 
-export class AuthGuardComponent extends React.Component {
-  constructor(props) {
-    super(props)
+const AuthGuardComponent = ({ currentUserId, children, authOrRedirect }) => {
+  const [loading, setLoading] = useState(true)
 
-    this.state = {
-      loading: true
-    }
-  }
-
-  async componentWillMount() {
-    const { authOrRedirect, currentUserId } = this.props
-
+  const attemptAuth = async () => {
     if (!currentUserId) {
-      const response = await authOrRedirect()
-
-      if (response && response.status === 200) {
-        this.setState({
-          loading: false
-        })
-      }
-    } else {
-      this.setState({
-        loading: false
-      })
+      await authOrRedirect()
     }
+
+    setLoading(false)
   }
 
-  render() {
-    const { children } = this.props
-    const { loading } = this.state
+  useEffect(() => {
+    attemptAuth()
+  }, [])
 
-    if (loading) {
-      return (
-        <div className="flex h-screen items-center">
-          <div className="w-screen text-3xl text-center text-grey">
-            Loading...
-          </div>
+  if (loading) {
+    return (
+      <div className="flex h-screen items-center">
+        <div className="w-screen text-3xl text-center text-grey">
+          Loading...
         </div>
-      )
-    }
-
-    return <div>{children}</div>
+      </div>
+    )
   }
+
+  return <Fragment>{children}</Fragment>
 }
 
-const mapStateToProps = state => ({
-  currentUserId: state.session.currentUser
-})
-
-const mapDispatchToProps = dispatch => ({
-  authOrRedirect: () => {
-    return dispatch(getCurrentUserInfo()).catch(() => {
-      dispatch(replace('/login'))
-    })
-  }
-})
-
 export const AuthGuard = connect(
-  mapStateToProps,
-  mapDispatchToProps
+  state => ({
+    currentUserId: state.session.currentUser
+  }),
+  dispatch => ({
+    authOrRedirect: () => {
+      return dispatch(getCurrentUserInfo()).catch(() => {
+        dispatch(replace('/login'))
+      })
+    }
+  })
 )(AuthGuardComponent)
