a7dd52fe7d4deb44d078718a0562865a54b8c4aa
Carwyn Stephen <caztephen@gmail.com>
2019-08-18 15:35:18 +0200

    Allow overriding input styles

--- resources/assets/js/components/Forms/Inputs.jsx
+++ resources/assets/js/components/Forms/Inputs.jsx
@@ -3,18 +3,28 @@ import React from 'react'
 const textInputClasses =
   'block w-full border border-grey-light bg-grey-lightest rounded'
 
-export const TextInput = props => (
-  <input className={`${textInputClasses} h-12 px-2`} {...props} type="text" />
+export const TextInput = ({ className = '', input, ...props }) => (
+  <input
+    className={`${textInputClasses} h-12 px-2 ${className}`}
+    {...input}
+    {...props}
+    type="text"
+  />
 )
 
-export const PasswordInput = props => (
+export const PasswordInput = ({ className = '', input, ...props }) => (
   <input
-    className={`${textInputClasses} h-12 px-2`}
+    className={`${textInputClasses} h-12 px-2 ${className}`}
+    {...input}
     {...props}
     type="password"
   />
 )
 
-export const TextArea = props => (
-  <textarea className={`${textInputClasses} h-48 p-2`} {...props} />
+export const TextArea = ({ className = '', input, ...props }) => (
+  <textarea
+    className={`${textInputClasses} h-48 p-2 ${className}`}
+    {...input}
+    {...props}
+  />
 )
